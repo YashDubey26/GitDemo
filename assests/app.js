2b05c4be1605ec2067581a2563507da4ac3edf2b
Yash Dubey <dyash9149@gmail.com>
2022-01-12 19:36:17 +0530

    Javascript Started

---
+++ assests/app.js
@@ -0,0 +1,77 @@
+// // // console.log("Hello Welcome to a Javascript");
+// // //window.alert("This is alert");
+
+// // //Number
+// // let age=23
+// // console.log(age);
+
+// // //String
+
+// // let brand='Yash';
+// // console.log(brand);
+
+// // console.log(age, (typeof age));
+// // console.log(brand, (typeof brand));
+
+// // Assignment Operator
+
+// let age=12;
+// console.log(age++);
+// console.log(age);
+
+
+// //comparison operators
+
+// let a =10
+// let b ='10'
+// console.log(a==b)
+// console.log(a<b)
+// console.log(a>=b)
+// console.log(a<=b)
+
+// // Arithmetic Operators
+
+// let n1=10;
+// let n2=20;
+// console.log(n1+n2);
+// console.log(n1-n2);
+// console.log(n1*n2);
+// console.log(n1/n2);
+// console.log(n1%n2);
+
+// // Logical Operators
+
+// console.log('true && true', true&&true);
+// console.log('true && false', true&&false);
+// console.log('false && true', false&&true);
+// console.log('false && false', false&&false);
+
+
+// console.log('true || true', true ||true);
+// console.log('true || false', true||false);
+// console.log('false || true', false||true);
+// console.log('false || false', false||false);
+
+
+//Functions
+// function greet(Name="defualtname"){
+//     console.log(Name)
+// }
+// greet()
+
+// function add(a=0, b=0){
+//     const c=a+b
+//     return c
+// }
+// sum=add(10,20)
+// console.log(sum)
+
+greet1()
+greet2()
+function greet1(){
+    console.log("Good Morning");
+}
+
+let greet2=function(){
+    console.log("Good Evening");
+}
