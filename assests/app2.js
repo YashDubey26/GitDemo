0eb35a643bef602604cda14cb1f565537780e552
Yash Dubey <dyash9149@gmail.com>
2022-01-13 19:41:05 +0530

    JS Day 2

---
+++ assests/app2.js
@@ -0,0 +1,53 @@
+// let brand="mercedes";
+// console.log(brand.toUpperCase());
+
+// //JSON
+// const temp={
+//     age:20,
+//     brand:'Mercedes',
+//     greet:()=>{console.log('hi')}
+// }
+// console.log(temp.age)
+// temp.greet()
+
+// literal notation to create an object
+// const car = {
+//     age: 18,
+//     brand: "Mercedes",
+//     getBrand: function() { console.log(this.age)}
+// }
+
+// // console.log(temp.age)
+// // console.log(temp['brand'])
+
+// car.getBrand()
+
+
+// console.log(window.innerWidth);
+// console.log(window.innerHeight);
+
+// // window.prompt("What is ur name");
+// console.warn("Warning");
+// console.error("Hey i am an error");
+
+
+//For Loop
+// let arr=[1,2,3,4,5]
+
+// for (let i=0; i<arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+// let arr=[1,2,3,4,5]
+
+// const callback=function(element){
+//     console.log(element)
+// }
+
+// arr.forEach(()=>{
+//     console.log('You called me')
+// })
+
+
+
+
