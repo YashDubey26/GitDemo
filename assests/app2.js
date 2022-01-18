786887c17a3d7f6fda9f50de090d51c5de51ac9e
Yash Dubey <dyash9149@gmail.com>
2022-01-18 19:38:17 +0530

    Day 16

--- assests/app2.js
+++ assests/app2.js
@@ -48,6 +48,34 @@
 //     console.log('You called me')
 // })
 
+// let elements = document.getElementsByClassName('temp')
+// let elements = document.getElementsByTagName('div')
+let elements=document.querySelectorAll('div.temp');
+
+let arr = [1, 2, 3, 4, 5]
+
+console.log(elements)
+console.log(arr)
 
 
+// for (let i = 0; i < elements.length; i++) {
+//     elements[i].innerHTML = `Element ${i}`
 
+// }
+
+elements.forEach((ele, i) => {
+    ele.innerHTML = `Element ${i}`
+});
+
+// const tasks=['GO to GYM', 'Take Medicine','Eat'];
+// const ol=document.createElement('ol')
+
+// tasks.forEach(task=>{
+//     const li=document.createElement("li")
+//     const textNode=document.createTextNode(task)
+//     li.appendChild(textNode)
+//     ol.appendChild(li)
+// })
+// console.log(ol)
+
+// body.appendChild(ol)
